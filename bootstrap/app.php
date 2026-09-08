2e8dd8ea37dce09d870f2663ac43a63c678ac587
john-Ross <johnhoundji@gmail.com>
2026-09-08 10:07:38 +0100

    Rediriger les invités vers register au lieu de login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->redirectGuestsTo('/register');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
